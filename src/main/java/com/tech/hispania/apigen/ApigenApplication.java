85d96b7eb004c5c1a852c08572a2dcb56f5a1948
Enrique Sanchez <esanchez.devel@gmail.com>
2025-10-22 16:46:52 +0200

    feat: create project

---
+++ src/main/java/com/tech/hispania/apigen/ApigenApplication.java
@@ -0,0 +1,13 @@
+package com.tech.hispania.apigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/hispania/apigen/ApigenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.hispania.apigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
