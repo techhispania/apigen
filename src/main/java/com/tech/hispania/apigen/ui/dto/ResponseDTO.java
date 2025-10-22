700523f991293f8448a52ce1d1a893577133b406
Enrique Sanchez <esanchez.devel@gmail.com>
2025-10-22 18:05:06 +0200

    feat: create endpoint to receive request from html form to generate the api

---
+++ src/main/java/com/tech/hispania/apigen/ui/dto/ResponseDTO.java
@@ -0,0 +1,19 @@
+package com.tech.hispania.apigen.ui.dto;
+
+public class ResponseDTO {
+
+	private String result;
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	@Override
+	public String toString() {
+		return "ResponseDTO [result=" + result + "]";
+	}
+}
