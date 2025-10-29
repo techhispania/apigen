68469a9a713a09a63ecd39706f9029171d0a8b62
Enrique Sanchez <esanchez.devel@gmail.com>
2025-10-29 17:48:32 +0100

    feat: create all needed packages and start to create files

--- src/main/java/com/tech/hispania/apigen/app/services/impl/ApiGenerationServiceImpl.java
+++ src/main/java/com/tech/hispania/apigen/app/services/impl/ApiGenerationServiceImpl.java
@@ -20,6 +20,10 @@ public class ApiGenerationServiceImpl implements ApiGenerationService {
 
 	private static final Logger logger = LogManager.getLogger(ApiGenerationServiceImpl.class);
 	
+	private static final String PROJECT_BASE_PATH = "api-rest/src/main/";
+	private static final String JAVA_BASE_PATH = new StringBuilder(PROJECT_BASE_PATH).append("java/com/apigen/").toString();
+	private static final String RESOURCES_BASE_PATH = new StringBuilder(PROJECT_BASE_PATH).append("resources/").toString();
+	
 	@Autowired
 	private FileSystemService fileSystemService;
 	
@@ -29,26 +33,56 @@ public class ApiGenerationServiceImpl implements ApiGenerationService {
 	@Override
 	public String generate(String apiName, Set<ApiGenEntity> entities) throws ApiGenException {
 		
+		String packageName = apiName.toLowerCase().replace(" ", "/");
+		
+		String javaPackage = new StringBuilder(JAVA_BASE_PATH).append(packageName).toString();
+		String controllersPackage = new StringBuilder(javaPackage).append("ui/controllers").toString();
+		String entitiesPackage = new StringBuilder(javaPackage).append("domain/model/entities").toString();
+		String servicesPackage = new StringBuilder(javaPackage).append("app/services").toString();
+		String servicesImplPackage = new StringBuilder(javaPackage).append("app/services/impl").toString();
+		String exceptionsPackage = new StringBuilder(javaPackage).append("app/exceptions").toString();
+		String persistencePackage = new StringBuilder(javaPackage).append("infrastructure/persistence").toString();
+		
 		fileSystemService.setTempDirectory(".");
 		
+		logger.info("===============================");
 		logger.info("Creating API packages structure");
-		fileSystemService.createRecursiveDirectory("api-rest/src/main/java/com/apigen/" + apiName);
-		fileSystemService.createRecursiveDirectory("api-rest/src/main/resources");
+		logger.info("===============================");
+		fileSystemService.createRecursiveDirectory(javaPackage);
+		fileSystemService.createRecursiveDirectory(controllersPackage);
+		fileSystemService.createRecursiveDirectory(entitiesPackage);
+		fileSystemService.createRecursiveDirectory(servicesImplPackage);
+		fileSystemService.createRecursiveDirectory(exceptionsPackage);
+		fileSystemService.createRecursiveDirectory(persistencePackage);
+		fileSystemService.createRecursiveDirectory(RESOURCES_BASE_PATH);
 		
-		logger.info("Creating Main file");
-		fileSystemService.createFile("api-rest/src/main/java/com/apigen/" + apiName, "App.java");
+		logger.info("===============================");
+		logger.info("Creating files");
+		logger.info("===============================");
+		fileSystemService.createFile(javaPackage, new StringBuilder(capitalize(apiName)).append("Application.java").toString());
+		fileSystemService.createFile(controllersPackage, "ApiController.java");
 		
+		logger.info("===============================");
 		logger.info("Copying content from template");
-		fileTemplateService.copyTemplateInFile("main", "api-rest/src/main/java/com/apigen/" + apiName + "/App.java");
+		logger.info("===============================");
+		fileTemplateService.copyTemplateInFile("main", "api-rest/src/main/java/com/apigen/" + packageName + "/App.java");
 		
+		logger.info("===============================");
 		logger.info("Replacing placeholders");
+		logger.info("===============================");
 		Map<String, String> placeholders = new HashMap<>();
 		placeholders.put("apiName", apiName);
-		fileTemplateService.replacePlaceholders("api-rest/src/main/java/com/apigen/" + apiName + "/App.java", placeholders);
+		fileTemplateService.replacePlaceholders("api-rest/src/main/java/com/apigen/" + packageName + "/App.java", placeholders);
 		
+		logger.info("===============================");
 		logger.info("Cleaning temporal directory");
+		logger.info("===============================");
 		fileSystemService.removeDirectory("api-rest");
 		
 		return null;
+	}
+	
+	private String capitalize(String text) {
+		return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
 	}
 }
