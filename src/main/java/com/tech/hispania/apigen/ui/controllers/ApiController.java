01a6956ff36afd60f9461874402f539044ddb335
Enrique Sanchez <esanchez.devel@gmail.com>
2025-10-29 18:12:34 +0100

    feat: #11 create mock data to test

--- src/main/java/com/tech/hispania/apigen/ui/controllers/ApiController.java
+++ src/main/java/com/tech/hispania/apigen/ui/controllers/ApiController.java
@@ -1,6 +1,7 @@
 package com.tech.hispania.apigen.ui.controllers;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tech.hispania.apigen.app.exceptions.ApiGenException;
 import com.tech.hispania.apigen.app.services.ApiGenerationService;
 import com.tech.hispania.apigen.domain.model.ApiGenEntity;
+import com.tech.hispania.apigen.domain.model.ApiGenProperty;
+import com.tech.hispania.apigen.domain.model.PropertyType;
 import com.tech.hispania.apigen.ui.dto.GenerateRequestDTO;
 import com.tech.hispania.apigen.ui.dto.GenerateResponseDTO;
 import com.tech.hispania.apigen.ui.dto.ResponseDTO;
@@ -33,9 +36,22 @@ public class ApiController {
 		logger.info("Generate REST API request received. {}", dto);
 		
 		try {
+			// =======================================
+			// TEST DATA
 			String apiName = "Planes";
 			Set<ApiGenEntity> entities = new HashSet<>();
 			
+			ApiGenProperty modelProp = new ApiGenProperty("model", PropertyType.TEXT);
+			ApiGenProperty companyProp = new ApiGenProperty("company", PropertyType.TEXT);
+			
+			List<ApiGenProperty> planeEntityProperties = List.of(modelProp, companyProp);
+			ApiGenEntity planeEntity = new ApiGenEntity("plane", planeEntityProperties);
+			
+			entities.add(planeEntity);
+
+			// END TEST DATA
+			// =======================================
+
 			String result = apiGenerationService.generate(apiName, entities);
 		} catch (ApiGenException e) {
 			logger.error("Error generating REST API.", e);
